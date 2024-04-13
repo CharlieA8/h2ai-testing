ea88c3c54444d0c1f2441d6e0e7788707d6f0d5a
Charlie <ceadorney@gmail.com>
2024-04-13 12:15:26 -0400

    removed json import

--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import json
 
 app = Flask(__name__)
 

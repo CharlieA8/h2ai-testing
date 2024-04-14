c17629bf64fd8f51671b85436f22d2b1d5dd49ed
Matthew Jordan <mdj82@georgetown.edu>
2024-04-13 22:20:12 -0400

    Added doctors page that uses that getDoctors route to display info

--- app.py
+++ app.py
@@ -2,25 +2,32 @@ from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from scraper import scrape_doctors
 
+from flask_cors import CORS
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+CORS(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 
-@app.route('/get-doctors', methods=['GET'])
+
+@app.route("/get-doctors", methods=["GET"])
 def get_doctors():
-    specialty = request.args.get('specialty')
-    location = request.args.get('location')
-    insurance_carrier = request.args.get('insurance_carrier')
-    
+    specialty = request.args.get("specialty")
+    location = request.args.get("location")
+    insurance_carrier = request.args.get("insurance_carrier")
+
     doctors_info = scrape_doctors(specialty, location, insurance_carrier)
     if doctors_info:
         return jsonify(doctors_info)
     else:
         return jsonify({"error": "No results"})
-    
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-if __name__ == '__main__':
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)

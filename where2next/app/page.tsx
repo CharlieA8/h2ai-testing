e497a79216cea00f2f73dd6c0fa35480d54c2ac3
Maggie-XShen <maggie.xinheshen@gmail.com>
2024-04-14 01:08:33 -0400

    delete extra btn (idk whats wrong)

--- where2next/app/page.tsx
+++ where2next/app/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "idk whats wrong", href: "/form" },
   { name: "i need help", href: "/info" },
 ];
 

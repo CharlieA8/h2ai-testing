d9e2fe54b58aca0bf36f00970188d84363305f2a
Maggie-XShen <maggie.xinheshen@gmail.com>
2024-04-14 05:00:46 -0400

    deleted extra btns

--- where2next/app/components/nav.tsx
+++ where2next/app/components/nav.tsx
@@ -28,7 +28,7 @@ export const Navigation: React.FC = () => {
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
 					<div className="flex justify-between gap-8">
-						<Link
+						{/* <Link
 							href="/llm"
 							className="duration-200 text-zinc-100 hover:underline"
 						>
@@ -39,7 +39,7 @@ export const Navigation: React.FC = () => {
 							className="duration-200 text-zinc-100 hover:underline"
 						>
 							Info Cards 
-						</Link>
+						</Link> */}
 
 					</div>
 

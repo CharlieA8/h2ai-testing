19885dbc19d542f03ddb8ed221e3c788f8a9811b
Maggie-XShen <maggie.xinheshen@gmail.com>
2024-04-14 03:34:33 -0400

    add storedZipCode function, fixed doctor page design

--- where2next/app/doctors/page.js
+++ where2next/app/doctors/page.js
@@ -1,10 +1,22 @@
-import React from 'react';
+"use client"
+import React, { useEffect, useState } from 'react';
 import DoctorCards from './doctorCards';
 import "./page.css";
 import Link from "next/link";
 
 
 function Page() {
+
+  const storedZipCode = localStorage.getItem("userZipCode");
+
+  const [userZipCode, setUserZipCode] = useState(storedZipCode || 20057);
+  
+  console.log(userZipCode);
+
+  useEffect(() => {
+    localStorage.setItem("userZipCode", userZipCode);
+  }, [userZipCode]);
+
   return (
     <div className='get-doctors-container'>
         <h1>Care near you!</h1>
